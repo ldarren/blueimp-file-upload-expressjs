88d322c5b7c4e47f35d307b9b5353a4c154d164d
Darren Liew <ldarren@gmail.com>
2018-05-19 18:06:28 +0800

    make unit test work again

--- specs/fileupload-spec.js
+++ specs/fileupload-spec.js
@@ -1,6 +1,6 @@
 'use strict';
 describe('FileInfo package', function() {
-  var FileInfo = require('../lib/fileinfo.js');
+  var FileInfo = require('../lib/fileinfo');
   // TODO - FileInfo default constructor or mock parameters
 
   it('should provide a safe name for new files');
@@ -13,12 +13,12 @@ describe('FileInfo package', function() {
 });
 
 describe('AWS transport package', function() {
-  var uploadFileAWS = require('../lib/transport/aws.js');
+  var uploadFileAWS = require('../lib/transport/aws');
 });
 
 describe('Uploader configuration', function() {
-  var options;
   var uploader;
+  var configs;
 
   beforeEach(function() {
     // TODO - Create a mock object for the filesystem
@@ -26,87 +26,74 @@ describe('Uploader configuration', function() {
   });
  
   it('can require configs without error',function(){
-    var configs = require('../lib/configs.js');
+    configs = require('../lib/configs');
     expect(configs).not.toBe(null);
   });
 
   it('should have default config values', function() {
-    options = {};
-    expect(uploader(options).config).toBeDefined();
+    expect(configs({
+		storages: [
+			{
+				type: 's3',
+				uploadDir: 'upload',
+				config:{
+					bucketName: 'bucket'
+				}
+			},{
+				type: 'local',
+				uploadDir: 'upload'
+			}
+		]
+	  })).toBeDefined();
   });
 
   it('should support the local filesystem', function() {
-    options = {
-      tmpDir: 'tmp/foo',
-      uploadDir: 'tmp/bar'
-    };
-    expect(uploader(options).config.tmpDir).toEqual('tmp/foo');
-    expect(uploader(options).config.uploadDir).toEqual('tmp/bar');
+    var opts = configs({
+		tmpDir: 'tmp/foo',
+		storages: [
+			{
+				type: 'local',
+				uploadDir: 'tmp/bar'
+			}
+		]
+	});
+    expect(opts.tmpDir).toEqual('tmp/foo');
+    expect(opts.storages[0].uploadDir).toEqual('tmp/bar');
   });
 
   it('should support Amazon Simple Storage Service', function() {
     var awsConfig = {
-      type: 'aws',
-      aws: {
-        accessKeyId: 'sesame',
-        secretAccessKey: 'open',
-        region: 'us-west-2',
+      type: 's3',
+      config: {
         bucketName: 'ali-baba',
         acl: 'private'
       }
     };
-    options = {
-      storage: awsConfig
+    var options = {
+      storages: [awsConfig]
     };
-    expect(uploader(options).config.storage).toEqual(awsConfig);
-  });
-
-  it('should support thumbnails generation', function() {
-    var thumbsConfig = {
-      maxWidth: 200,
-      maxHeight: 'auto',
-      large: {
-        width: 600,
-        height: 600
-      },
-      medium: {
-        width: 300,
-        height: 300
-      },
-      small: {
-        width: 150,
-        height: 150
-      }
-    }
-    options = {
-      imageVersions: thumbsConfig
-    };
-    var obj = uploader(options);
-    
-    expect(obj.config.imageVersions.thumbnail.width).toEqual(thumbsConfig.maxWidth);
-    expect(obj.config.imageVersions.thumbnail.height).toEqual(thumbsConfig.maxHeight);
-    expect(obj.config.imageVersions.large).toEqual(thumbsConfig.large);
-    expect(obj.config.imageVersions.medium).toEqual(thumbsConfig.medium);
-    expect(obj.config.imageVersions.small).toEqual(thumbsConfig.small);
-  });
-
-  it('should allow disabling thumbnails', function() {
-    options = {
-      copyImgAsThumb: true
-    };
-    expect(uploader(options).config.copyImgAsThumb).toBe(true);
+    expect(configs(options).storages[0]).toEqual(awsConfig);
   });
 
   it('should support SSL', function() {
-    options = {
-      useSSL: true
+    var options = {
+      useSSL: true,
+	  storages:[{
+	  	type: 'local',
+		uploadDir: 'tmp'
+	  }]
     };
-    expect(uploader(options).config.useSSL).toBe(true);
+    expect(configs(options).useSSL).toBe(true);
   });
 });
 
 describe('Uploader REST services', function() {
-  var options = {};
+  var options = {
+  	storages:[{
+		type: 'local',
+		uploadDir: 'tmp'
+	}]
+  };
   var uploader = require('../index')(options);
 
   it('should provide a GET method', function() {
